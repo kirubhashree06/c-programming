3b2917e327f06ac9d48984797eb35268cc38bb35
kirubhashree06 <kirubhashreesenthilkumar67@gmail.com>
2026-01-25 14:52:25 +0530

    Add files via upload
    solved the C programming problem "Divide two variables"

---
+++ Sample17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a = 8;
+    int b = 2;
+    int c = 10;
+    int d = 2;
+    int div = a/b;
+    printf("%d\n",div);
+    printf("%d",c/d);
+    return 0;
+}
