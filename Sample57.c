1845b09d49c606387020d9021ff694c8452c8e4b
Kirubhashree S <kirubhashreesenthilkumar67@gmail.com>
2026-01-26 19:13:51 +0530

    Add files via upload
    solved the C programming problem "Find Greater of Two Numbers"

---
+++ Sample57.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int k,s;
+    scanf("%d %d",&k,&s);
+    if(k>s){
+        printf("%d is greater",k);
+    } else{
+        printf("%d is greater",s);
+    }
+    return 0;
+}
