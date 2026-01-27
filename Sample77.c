1304c09633cab9f8e58cfcef2694805b44f35ede
Kirubhashree S <kirubhashreesenthilkumar67@gmail.com>
2026-01-27 11:39:43 +0530

    Add files via upload
    solved the C programming problem "Vowel Using Switch"

---
+++ Sample77.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+ 
+    char s;
+   scanf("%c",&s);
+    switch(op){
+        case 'a': case 'e': case 'i': case 'o': case 'u': case 'A': case 'E': case 'I': case 'O': case 'U':
+        printf("vowles");
+        break;
+        
+    }
+    return 0;
+    
+}
