060e1f0e80c8a98b841dc35396630c9336691aa4
Kirubhashree S <kirubhashreesenthilkumar67@gmail.com>
2026-01-29 09:09:59 +0530

    Add files via upload
    solved the c programming problem"Write a program to convert a binary number into its decimal equivalent using a for loop"

---
+++ sample21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, digit, decimal = 0, base = 1;
+
+    scanf("%d", &n);
+
+    for (; n > 0; n = n / 10) {
+        digit = n % 10;
+        decimal = decimal + (digit * base);
+        base = base * 2;
+    }
+
+    printf("%d", decimal);
+    return 0;
+}
