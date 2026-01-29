299b05bdcd9763ad179d3f84a2bfa3d1e298ff7f
Kirubhashree S <kirubhashreesenthilkumar67@gmail.com>
2026-01-30 04:25:08 +0530

    Add files via upload

---
+++ nested loop/sam19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    int n = 5;
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        for (space = 1; space <= (n - i) * 2 + 1; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        for (space = 1; space <= (n - i) * 2 + 1; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
